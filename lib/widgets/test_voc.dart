fbecca5e80e486e322750baf576015b313767126
Edwin Hurtado Almeida <104462132+Hedurtado@users.noreply.github.com>
2023-10-29 09:21:11 -0500

    class voc1

--- lib/widgets/test_voc.dart
+++ lib/widgets/test_voc.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dots_indicator/dots_indicator.dart';
-import 'package:video_player/video_player.dart';
 
 class Voc1 extends StatefulWidget {
   final BuildContext context;
@@ -11,212 +8,23 @@ class Voc1 extends StatefulWidget {
   final String title;
   final String description;
   final int index;
-  const Voc1(
-      {required this.context,
-      required this.image,
-      required this.meaning,
-      required this.title,
-      required this.description,
-      required this.index,
-      super.key});
 
+  const Voc1({
+    super.key,
+    required this.context,
+    required this.image,
+    required this.meaning,
+    required this.title,
+    required this.description,
+    required this.index,
+  });
   @override
-  // ignore: library_private_types_in_public_api
-  _VideosStateTest createState() => _VideosStateTest();
+  State<Voc1> createState() => _Voc1State();
 }
 
-class _VideosStateTest extends State<Voc1> {
-  int currentIndex = 0;
-  int otherIndex = 0;
-  late VideoPlayerController _controller;
-  late Future<void>
-      // ignore: unused_field
-      _controllerInitializer; // Variable para la inicialización del controlador
-  List<Map<String, String>> videos = [];
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Iniciar la inicialización del controlador en el initState
-    _getVideosFromFirestore();
-  }
-
-  Future<void> _getVideosFromFirestore() async {
-    CollectionReference collection =
-        FirebaseFirestore.instance.collection('Videos');
-    QuerySnapshot querySnapshot = await collection.get();
-
-    List<Map<String, String>> fetchvideos = [];
-
-    for (var doc in querySnapshot.docs) {
-      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-      String title = data['title'];
-      String url = data['url'];
-      fetchvideos.add({
-        'title': title,
-        'url': url,
-        'videoId': doc.id
-      }); // Usar doc.id como identificador
-    }
-
-    setState(() {
-      videos = fetchvideos;
-      videos.sort(((a, b) => a['title']!.compareTo(b['title']!)));
-    });
-
-    _initializeController();
-  }
-
-  Future<void> _initializeController() async {
-    String? videoUrl = videos[currentIndex]['url'];
-
-    if (videoUrl != null) {
-      _controller = VideoPlayerController.networkUrl(Uri.parse(videoUrl));
-
-      await _controller.initialize();
-      await _controller.setLooping(true);
-      await _controller.play();
-    }
-  }
-
-  void moveToNext() {
-    setState(() {
-      if (currentIndex < videos.length - 1) {
-        currentIndex = currentIndex + 1;
-        otherIndex = currentIndex;
-        _controller.dispose();
-        _controller = VideoPlayerController.networkUrl(
-            Uri.parse(videos[currentIndex]['url']!));
-        _controller.initialize().then((_) {
-          _controller.play();
-        });
-      }
-    });
-  }
-
-  void moveToPrev() {
-    setState(() {
-      if (currentIndex > 0) {
-        currentIndex = currentIndex - 1;
-        otherIndex = currentIndex;
-        _controller.dispose();
-        _controller = VideoPlayerController.networkUrl(
-            Uri.parse(videos[currentIndex]['url']!));
-        _controller.initialize().then((_) {
-          _controller.play();
-        });
-      } else if (currentIndex == 0) {
-        currentIndex = videos.length - 1;
-        otherIndex = currentIndex;
-        _controller.dispose();
-        _controller = VideoPlayerController.networkUrl(
-            Uri.parse(videos[currentIndex]['url']!));
-        _controller.initialize().then((_) {
-          _controller.play();
-        });
-      }
-    });
-  }
-
-  Widget buildButtonOptions(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: MediaQuery.of(context).size.height * 0.05,
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            margin: EdgeInsets.only(
-              left: MediaQuery.of(context).size.width * 0.1,
-            ),
-            child: InkWell(
-              onTap: moveToPrev,
-              child: Image.asset(
-                'assets/double_left.png',
-                width: 50,
-                height: 50,
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(
-              right: MediaQuery.of(context).size.width * 0.1,
-              left: MediaQuery.of(context).size.width * 0.1,
-            ),
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, 'abecedario');
-                _controller.pause();
-              },
-              child: Image.asset(
-                'assets/home.png',
-                width: 50,
-                height: 50,
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(
-              right: MediaQuery.of(context).size.width * 0.1,
-            ),
-            child: InkWell(
-              onTap: moveToNext,
-              child: Image.asset(
-                'assets/double_right.png',
-                width: 50,
-                height: 50,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildVideo(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(
-        top: MediaQuery.of(context).size.height * 0.01,
-        bottom: MediaQuery.of(context).size.height * 0.03,
-      ),
-      width: MediaQuery.of(context).size.width * 0.7,
-      height: MediaQuery.of(context).size.height * 0.5,
-      child: AspectRatio(
-        aspectRatio: _controller.value.aspectRatio,
-        child: VideoPlayer(_controller),
-      ),
-    );
-  }
-
-  Widget buildTitulo(BuildContext context) {
-    final videoName = videos[currentIndex]['title'];
-    return Container(
-      margin: EdgeInsets.only(
-        top: MediaQuery.of(context).size.height * 0.01,
-        bottom: MediaQuery.of(context).size.height * 0.02,
-      ),
-      child: Text(
-        "Esta seña representa la letra \n$videoName",
-        style: const TextStyle(
-          fontSize: 26,
-          fontWeight: FontWeight.bold,
-          fontFamily: 'istokWeb',
-        ),
-      ),
-    );
-  }
-
+class _Voc1State extends State<Voc1> {
   @override
   Widget build(BuildContext context) {
-    if (videos.isEmpty) {
-      return Container(
-          color: Colors.white,
-          child: const Center(
-            child: CircularProgressIndicator(color: Colors.purple),
-          ));
-    }
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -249,8 +57,13 @@ class _VideosStateTest extends State<Voc1> {
                 flex: 7,
                 child: Center(
                   child: Container(
-                      alignment: Alignment.center,
-                      child: buildVideo(widget.context)),
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      widget.image,
+                      fit: BoxFit.contain,
+                      height: double.infinity,
+                    ),
+                  ),
                 ),
               ),
             ],
